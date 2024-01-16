3a4ef35c2aa6e550a889895e5fd82ee7c6091a4f
Yoclar <thereallemonmail@gmail.com>
2024-01-16 08:15:47 +0100

    Hero and Randomizer class added

---
+++ Adventurer/Hero.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventurer
+{
+    internal class Hero :Randomizer
+    {
+        protected int MaxHp;
+        protected int ActualHp;
+        protected int DefensePoint;
+        protected int Damage;
+
+
+        public Hero()
+        {
+            MaxHp = 20 + 3 * RandomNum();
+            MaxHp = ActualHp;
+            DefensePoint = 2 * RandomNum();
+            Damage = 5 + RandomNum();
+        }
+
+        protected void LevelUp()
+        {
+            MaxHp = MaxHp += RandomNum();
+            DefensePoint += RandomNum();
+            Damage += RandomNum();
+
+        }
+    }
+}
---
+++ Adventurer/Randomizer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventurer
+{
+    internal class Randomizer
+    {
+        protected int RandomNum()
+        {
+            Random r = new Random();
+            return r.Next(1,7);
+        }
+    }
+}
